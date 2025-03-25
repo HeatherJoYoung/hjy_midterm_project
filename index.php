e13f6243f4ec644bc8ffb15fd881f64f869af2f3
hjyoung <hjy.earth@gmail.com>
2025-03-25 16:35:41 -0600

    adds index file to root

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php     
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INF653 Midterm Project</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>INF653 Midterm Project</h1>
+				<h4>Heather Young</h4>
+    </header>
+</body>
+
+</html>
